c9fbded8e75523fafd78e7e7f468e9a7b709b5b3
Your Name <you@example.com>
2026-04-21 14:31:44 +0300

    refaktored the controller method so that it does not give the db connection as a parameter, but uses the DB class query function

--- public/index.php
+++ public/index.php
@@ -4,10 +4,9 @@ require_once __DIR__ . '/../db/DB.php';
 require_once __DIR__ . '/../src/controllers/CustomerController.php';
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$requestUri = rtrim($requestUri , '/') ?: '/';
 
-if ($requestUri  === '/customers') {
+if ($requestUri === '/customers') {
+
     CustomerController::index();
-} else {
-    echo "404";
+
 }
--- src/controllers/CustomerController.php
+++ src/controllers/CustomerController.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once __DIR__ . '/../../db/DB.php';
 
 class CustomerController {
@@ -9,8 +10,22 @@ class CustomerController {
 
         echo "<h1>Customers</h1>";
 
+        echo "<table border='1' style='width:80%; font-size:18px; border-collapse: collapse;'>";
+
+        echo "<tr style='background:#f2f2f2;'>
+                <th style='padding:10px;'>ID</th>
+                <th style='padding:10px;'>Name</th>
+                <th style='padding:10px;'>Email</th>
+              </tr>";
+
         foreach ($result as $row) {
-            echo $row['first_name'] . " - " . $row['email'] . "<br>";
+            echo "<tr>";
+            echo "<td style='padding:10px;'>" . htmlspecialchars($row['customer_id']) . "</td>";
+            echo "<td style='padding:10px;'>" . htmlspecialchars($row['first_name']) . "</td>";
+            echo "<td style='padding:10px;'>" . htmlspecialchars($row['email']) . "</td>";
+            echo "</tr>";
         }
+
+        echo "</table>";
     }
 }
